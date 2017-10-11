96b0474874a5681ec0e712c833fbd5de1f0441fa
Rafal Wilinski <raf.wilinski@gmail.com>
2017-10-11 18:07:04 +0200

    Add serverless dockerfile

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node:8.6
+
+RUN ["npm", "install", "-g", "serverless"]
--- run.sh
+++ run.sh
@@ -1,50 +1,28 @@
-#!/bin/bash -u
+# Install Serverless Framework
+npm install -g serverless
 
-# AWS Batch Command
+# Clone repository and change directory
+git clone $REPO_URL
+cd $REPO_NAME
 
-set -e
+# Install dependencies
+echo 'Installing dependencies...'
+npm install
 
-run() {
-  echo 'Starting processing job...'
-  (process) || {
-    exit_code=$?
-    echo 'Process exited with error'
-    return $exit_code
-  }
+# Apply optional build commands like babel or webpack
+eval $BUILD_COMMAND
 
-  echo 'Process finished successfully!'
-}
+# Run `serverless package`, this might be overriden
+eval $PACKAGE_COMMAND
 
-process() {
-  # Install Serverless Framework
-  npm install -g serverless
+# Go to artifacts & compiled Cloudformation template path
+cd .serverless
 
-  # Clone repository and change directory
-  git clone $REPO_URL
-  cd $REPO_NAME
+echo 'Creating upload buckets...'
+aws s3 create-bucket --acl public-read --bucket $ARTIFACTS_BUCKET_NAME
+aws s3 create-bucket --acl public-read --bucket $TEMPLATE_BUCKET_NAME
 
-  # Install dependencies
-  echo 'Installing dependencies...'
-  npm install
-
-  # Apply optional build commands like babel or webpack
-  $BUILD_COMMAND
-
-  # Run `serverless package`, this might be overriden
-  $PACKAGE_COMMAND
-
-  # Go to artifacts & compiled Cloudformation template path
-  cd .serverless
-
-  echo 'Creating upload buckets...'
-  aws s3 create-bucket --acl public-read --bucket $ARTIFACTS_BUCKET_NAME
-  aws s3 create-bucket --acl public-read --bucket $TEMPLATE_BUCKET_NAME
-
-  echo 'Uploading CFN template...'
+echo 'Uploading CFN template...'
 
 
-  echo 'Uploading artifacts...'
-
-}
-
-run
+echo 'Uploading artifacts...'
